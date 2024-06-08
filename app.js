f7c4b50e777e57d2b7f772795c812b3a462275be
egasa21 <esaputra168@gmail.com>
2024-06-08 15:21:49 +0700

    feat: use router in app.js

--- app.js
+++ app.js
@@ -7,14 +7,18 @@ let cors = require('cors')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+let authRouter = require('./routes/AuthRoutes');
 let customerRouter = require('./routes/CustomerRoutes');
 let productRouter = require('./routes/ProductRoutes');
 let categoryRouter = require('./routes/CategoryRoutes');
 let supplierRouter = require('./routes/SupplierRoutes');
-let authRouter = require('./routes/AuthRoutes');
 let employeeRouter = require('./routes/EmployeeRoutes');
+let orderRouter = require('./routes/OrderRoutes');
+let orderDetailRouter = require('./routes/OrderDetailRoutes');
+let shipperRouter = require('./routes/ShipperRoutes');
 
 const db = require('./models');
+const {or} = require("sequelize");
 
 const app = express();
 
@@ -25,7 +29,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(cors())
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -36,34 +40,37 @@ app.use('/api', categoryRouter);
 app.use('/api', supplierRouter);
 app.use('/api', productRouter);
 app.use('/api', employeeRouter);
+app.use('/api', orderRouter);
+app.use('/api', orderDetailRouter);
+app.use('/api', shipperRouter);
 app.use('/api/auth', authRouter);
 
 
 const syncDB = async () => {
-  try {
-    await db.sequelize.sync()
-    console.log('Database synced successfully')
-  } catch (error) {
-    console.log(error)
-  }
+    try {
+        await db.sequelize.sync()
+        console.log('Database synced successfully')
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 // syncDB()
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
